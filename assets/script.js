33707eb648972bcabd524d941031786ba11054bd
cassidytrbl <97814148+cassidytrbl@users.noreply.github.com>
2022-03-17 22:04:57 -0700

    adding filess

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+var start = document.createElement('button')
+
+const myQuestions = [
+    {
+        question: "What are the Little Twin Stars names?",
+        answers: {
+            a: "Nana and Hachi",
+            b: "Kiki and Lala",
+            c: "Blue and Pink"
+        },
+        correctAnswer: "b"
+    }
+]
